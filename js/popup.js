af2ae810da669a0926a61c416f18d223511ed24c
Liana Mina <lianazmina@gmail.com>
2023-04-11 23:52:30 -0500

    edits on formatting

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,20 @@
 
+import React from 'react'
+import { Message } from 'semantic-ui-react'
 
+document.getElementById("root").MessageExampleMessage;
+
+const MessageExampleMessage = () => (
+  <Message>
+    <Message.Header>Changes in Service</Message.Header>
+    <p>
+      We updated our privacy policy here to better service our customers. We
+      recommend reviewing the changes.
+    </p>
+  </Message>
+)
+
+export default MessageExampleMessage
 
 
 /*
